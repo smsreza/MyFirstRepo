b344800e489201e02509460144f7d3f0755b5d81
smsreza <46434796+smsreza@users.noreply.github.com>
2023-06-04 20:10:53 +1000

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+  public static void main(String[] args) {
+    // displays "Hello Everyone from Shaheen" in console
+    System.out.println("Hello Everyone from Shaheen");
+  }
+}
